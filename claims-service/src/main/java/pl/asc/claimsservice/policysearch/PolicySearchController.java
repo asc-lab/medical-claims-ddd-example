3761bd2becd58292a94df454091e8035582ee87c
kozemkop <pavlo.kozemko@altkom.pl>
2018-04-12 13:56:18 +0200

    claim-init

---
+++ claims-service/src/main/java/pl/asc/claimsservice/policysearch/PolicySearchController.java
@@ -0,0 +1,7 @@
+package pl.asc.claimsservice.policysearch;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PolicySearchController {
+}
---
+++ claims-service/src/main/java/pl/asc/claimsservice/policysearch/PolicySearchResponse.java
@@ -0,0 +1,13 @@
+package pl.asc.claimsservice.policysearch;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PolicySearchResponse {
+}
