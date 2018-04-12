65825817ea8d1991caed538e6e3c96884d7b648b
kozemkop <pavlo.kozemko@altkom.pl>
2018-04-12 13:34:10 +0200

    init

--- claims-service/src/main/java/pl/asc/claims/ClaimsServiceApplication.java
+++ claims-service/src/main/java/pl/asc/claims/ClaimsServiceApplication.java
@@ -1,7 +1,7 @@
 package pl.asc.claims;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClaimsServiceApplication {
---
+++ claims-service/src/main/java/pl/asc/claims/domain/Claim.java
@@ -0,0 +1,14 @@
+package pl.asc.claims.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Claim {
+
+
+}
