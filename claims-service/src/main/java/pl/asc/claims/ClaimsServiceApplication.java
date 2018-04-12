0f03fc2e8800c8447873e5d4842d2475a5542dcc
SOFTWARE\wsuwala <wojciech.suwala@altkom.pl>
2018-04-12 13:03:37 +0200

    initial commit

---
+++ claims-service/src/main/java/pl/asc/claims/ClaimsServiceApplication.java
@@ -0,0 +1,12 @@
+package pl.asc.claims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsServiceApplication.class, args);
+	}
+}
