d82f084ac569e671a479eb598237b318e1870c04
Arek Rosłoniec <arkadiusz.rosloniec@altkom.pl>
2018-06-27 08:23:10 +0200

    spock test

--- claims-service/src/test/groovy/pl/asc/claimsservice/domainmodel/CoPaymentSpec.groovy
+++ claims-service/src/test/groovy/pl/asc/claimsservice/domainmodel/CoPaymentSpec.groovy
@@ -5,39 +5,15 @@ import pl.asc.claimsservice.shared.primitives.Percent
 import spock.lang.Specification
 
 class CoPaymentSpec extends Specification {
-    void "given empty co-payment customer pays zero"() {
-        given:
-        def coPay = CoPayment.empty()
-        when:
-        def result = coPay.calculate(MonetaryAmount.from(100))
-        then:
-        result.amount == 0
-    }
-
-    void "given 10 percent co-payment customer pays 10"() {
-        given:
-        def coPay = CoPayment.percent(Percent.of(10))
-        when:
-        def result = coPay.calculate(MonetaryAmount.from(100))
-        then:
-        result.amount == 10
-    }
-
-    void "given 15 amount co-payment customer pays 15"() {
-        given:
-        def coPay = CoPayment.amount(MonetaryAmount.from(15))
-        when:
-        def result = coPay.calculate(MonetaryAmount.from(100))
-        then:
-        result.amount == 15
-    }
-
-    void "given 150 amount co-payment customer when requested amount below co-payment customer pays all"() {
-        given:
-        def coPay = CoPayment.amount(MonetaryAmount.from(150))
-        when:
-        def result = coPay.calculate(MonetaryAmount.from(100))
-        then:
-        result.amount == 100
+    void "customers pay for given co-payment"(CoPayment coPay, BigDecimal amount) {
+        expect:
+            def result = coPay.calculate(MonetaryAmount.from(100))
+            result.amount == amount
+        where:
+            coPay                                      | amount
+            CoPayment.empty()                          | 0
+            CoPayment.percent(Percent.of(10))          | 10
+            CoPayment.amount(MonetaryAmount.from(15))  | 15
+            CoPayment.amount(MonetaryAmount.from(150)) | 100
     }
 }
--- claims-service/src/test/groovy/pl/asc/claimsservice/domainmodel/LimitSpec.groovy
+++ claims-service/src/test/groovy/pl/asc/claimsservice/domainmodel/LimitSpec.groovy
@@ -5,172 +5,39 @@ import pl.asc.claimsservice.shared.primitives.Quantity
 import spock.lang.Specification
 
 class LimitSpec extends Specification {
-    // amount limit tests
 
-    void "when requested amount is below limit insurer pays all"() {
-        given: "given 100 amount for policy year"
-        def limit = Limit.amountForPolicyYear(MonetaryAmount.from(100))
-
-        when: "previous consumption is 0 and we try to take 90"
-        def paidByInsurer = limit.calculate(
-                Quantity.of(1),
-                MonetaryAmount.from(90),
-                MonetaryAmount.zero(),
-                Quantity.zero(),
-                MonetaryAmount.zero())
-
-        then: "all 90 is paid"
-        paidByInsurer.amount == 90
+    void "amount limit tests"(Quantity qt, MonetaryAmount price, MonetaryAmount coPayment, Quantity consumedQt,
+                              MonetaryAmount consumedAmount, BigDecimal amount) {
+        expect:
+            def limit = Limit.amountForPolicyYear(MonetaryAmount.from(100))
+            def paidByInsurer = limit.calculate(qt, price, coPayment, consumedQt, consumedAmount)
+            paidByInsurer.amount == amount
+        where:
+            qt             | price                    | coPayment             | consumedQt      | consumedAmount           | amount
+            Quantity.of(1) | MonetaryAmount.from(90)  | MonetaryAmount.zero() | Quantity.zero() | MonetaryAmount.zero()    | 90
+            Quantity.of(1) | MonetaryAmount.from(120) | MonetaryAmount.zero() | Quantity.zero() | MonetaryAmount.zero()    | 100
+            Quantity.of(1) | MonetaryAmount.from(90)  | MonetaryAmount.zero() | Quantity.zero() | MonetaryAmount.from(10)  | 90
+            Quantity.of(1) | MonetaryAmount.from(120) | MonetaryAmount.zero() | Quantity.zero() | MonetaryAmount.from(20)  | 80
+            Quantity.of(1) | MonetaryAmount.from(120) | MonetaryAmount.zero() | Quantity.zero() | MonetaryAmount.from(120) | 0
     }
 
-    void "when requested amount is above limit insurer pays up to limit"() {
-        given: "given 100 amount for policy year"
-        def limit = Limit.amountForPolicyYear(MonetaryAmount.from(100))
+    void "qt limit tests"(Quantity qt, MonetaryAmount price, MonetaryAmount coPayment, Quantity consumedQt,
+                          MonetaryAmount consumedAmount, BigDecimal amount) {
+        expect:
+            def limit = Limit.quantityForPolicyYear(Quantity.of(10))
+            def paidByInsurer = limit.calculate(qt, price, coPayment, consumedQt, consumedAmount)
+            paidByInsurer.amount == amount
+        where:
+            qt              | price                    | coPayment             | consumedQt      | consumedAmount        | amount
 
-        when: "previous consumption is 0 and we try to take 120"
-        def paidByInsurer = limit.calculate(
-                Quantity.of(1),
-                MonetaryAmount.from(120),
-                MonetaryAmount.zero(),
-                Quantity.zero(),
-                MonetaryAmount.zero())
-
-        then: "only 100 is paid"
-        paidByInsurer.amount == 100
-    }
-
-    void "when requested amount plus perv consumption is below limit insurer pays all"() {
-        given: "given 100 amount for policy year"
-        def limit = Limit.amountForPolicyYear(MonetaryAmount.from(100))
-
-        when: "previous consumption is 10 and we try to take 90"
-        def paidByInsurer = limit.calculate(
-                Quantity.of(1),
-                MonetaryAmount.from(90),
-                MonetaryAmount.zero(),
-                Quantity.zero(),
-                MonetaryAmount.from(10))
-
-        then: "all 90 is paid"
-        paidByInsurer.amount == 90
-    }
-
-    void "when requested amount plus prev consumption is above limit insurer pays up to limit"() {
-        given: "given 100 amount for policy year"
-        def limit = Limit.amountForPolicyYear(MonetaryAmount.from(100))
-
-        when: "previous consumption is 20 and we try to take 120"
-        def paidByInsurer = limit.calculate(
-                Quantity.of(1),
-                MonetaryAmount.from(120),
-                MonetaryAmount.zero(),
-                Quantity.zero(),
-                MonetaryAmount.from(20))
-
-        then: "only 80 is paid"
-        paidByInsurer.amount == 80
-    }
-
-
-    void "when limit exhausted by prev consumptions insurer pays zero"() {
-        given: "given 100 amount for policy year"
-        def limit = Limit.amountForPolicyYear(MonetaryAmount.from(100))
-
-        when: "previous consumption is 120 and we try to take 120"
-        def paidByInsurer = limit.calculate(
-                Quantity.of(1),
-                MonetaryAmount.from(120),
-                MonetaryAmount.zero(),
-                Quantity.zero(),
-                MonetaryAmount.from(120))
-
-        then: "only 80 is paid"
-        paidByInsurer.amount == 0
-    }
-
-    //qt limit tests
-    void "when requested qt is below limit insurer pays all"() {
-        given: "given 10 qt for policy year"
-        def limit = Limit.quantityForPolicyYear(Quantity.of(10))
-
-        when: "previous consumption is 0 and we try to take 9"
-        def paidByInsurer = limit.calculate(
-                Quantity.of(9),
-                MonetaryAmount.from(100),
-                MonetaryAmount.zero(),
-                Quantity.zero(),
-                MonetaryAmount.zero())
-
-        then: "all 900 is paid"
-        paidByInsurer.amount == 900
-    }
-
-    void "when requested qt is above limit insurer pays up to limit"() {
-        given: "given 10 qt for policy year"
-        def limit = Limit.quantityForPolicyYear(Quantity.of(10))
-
-        when: "previous consumption is 0 and we try to take 11"
-        def paidByInsurer = limit.calculate(
-                Quantity.of(11),
-                MonetaryAmount.from(100),
-                MonetaryAmount.zero(),
-                Quantity.zero(),
-                MonetaryAmount.zero())
-
-        then: "only 1000 is paid"
-        paidByInsurer.amount == 1000
-    }
-
-    void "when requested qt plus prev consumption is below limit insurer pays all"() {
-        given: "given 10 qt for policy year"
-        def limit = Limit.quantityForPolicyYear(Quantity.of(10))
-
-        when: "previous consumption is 1 and we try to take 9"
-        def paidByInsurer = limit.calculate(
-                Quantity.of(9),
-                MonetaryAmount.from(100),
-                MonetaryAmount.zero(),
-                Quantity.of(1),
-                MonetaryAmount.zero())
-
-        then: "all 900 is paid"
-        paidByInsurer.amount == 900
-    }
-
-    void "when requested qt plus prev consumption is above limit insurer pays up to limit"() {
-        given: "given 10 qt for policy year"
-        def limit = Limit.quantityForPolicyYear(Quantity.of(10))
-
-        when: "previous consumption is 2 and we try to take 9"
-        def paidByInsurer = limit.calculate(
-                Quantity.of(9),
-                MonetaryAmount.from(100),
-                MonetaryAmount.zero(),
-                Quantity.of(2),
-                MonetaryAmount.zero())
-
-        then: "only 800 is paid"
-        paidByInsurer.amount == 800
-    }
-
-    void "when limit is exhausted by prev qt consumption insurer pays zero"() {
-        given: "given 10 qt for policy year"
-        def limit = Limit.quantityForPolicyYear(Quantity.of(10))
-
-        when: "previous consumption is 10 and we try to take 9"
-        def paidByInsurer = limit.calculate(
-                Quantity.of(9),
-                MonetaryAmount.from(100),
-                MonetaryAmount.zero(),
-                Quantity.of(10),
-                MonetaryAmount.zero())
-
-        then: "zero is paid"
-        paidByInsurer.amount == 0
+            Quantity.of(9)  | MonetaryAmount.from(100) | MonetaryAmount.zero() | Quantity.zero() | MonetaryAmount.zero() | 900
+            Quantity.of(11) | MonetaryAmount.from(100) | MonetaryAmount.zero() | Quantity.zero() | MonetaryAmount.zero() | 1000
+            Quantity.of(11) | MonetaryAmount.from(100) | MonetaryAmount.zero() | Quantity.of(1)  | MonetaryAmount.zero() | 900
+            Quantity.of(11) | MonetaryAmount.from(100) | MonetaryAmount.zero() | Quantity.of(2)  | MonetaryAmount.zero() | 800
+            Quantity.of(11) | MonetaryAmount.from(100) | MonetaryAmount.zero() | Quantity.of(10) | MonetaryAmount.zero() | 0
     }
 
     //co payment test
-
 
     //amount and qt test
 }
